e9e0dabc90400051f1a8eed586863412bb5efa21
Kaes Xander <xander.kaes@student.arteveldehs.be>
2019-12-23 17:39:23 +0100

    Up button Footer

--- js/bootstrap-4.3.1.js
+++ js/bootstrap-4.3.1.js
@@ -74,6 +74,29 @@
    * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
    * --------------------------------------------------------------------------
    */
+                                       
+                                       
+                                       $(document).ready(function(){
+     $(window).scroll(function () {
+            if ($(this).scrollTop() > 50) {
+                $('#back-to-top').fadeIn();
+            } else {
+                $('#back-to-top').fadeOut();
+            }
+        });
+        // scroll body to 0px on click
+        $('#back-to-top').click(function () {
+            $('#back-to-top').tooltip('hide');
+            $('body,html').animate({
+                scrollTop: 0
+            }, 800);
+            return false;
+        });
+        
+        $('#back-to-top').tooltip('show');
+
+});
+                                       
   /**
    * ------------------------------------------------------------------------
    * Private TransitionEnd Helpers
